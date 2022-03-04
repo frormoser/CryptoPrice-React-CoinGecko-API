9e0bc2f29aafbd695512eef8ce69cc48a2680155
Federico <joelrormoser@gmail.com>
2022-03-04 11:25:15 -0300

    all values ​​are listed with their image

--- src/components/CoinRow.js
+++ src/components/CoinRow.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const CoinRow = ({coin, index}) => {
-    console.log(coin,index)
+const CoinRow = ({ coin, index }) => {
+    console.log(coin,index);
     return (
-        <tr key={coin.name}> 
-        <td>{coin.name}</td>
+        <tr> 
+            <td>{index}</td>
+            <td>
+                <img src={coin.image} alt={coin.name} style={{width: '3%'}} classname='img-fluid me-4' />
+                <span>
+                {coin.name}
+                </span>
+            </td>
+            <td>{coin.current_price}</td>
+            <td>{coin.price_change_percentage_24h}</td>
+            <td>{coin.total_volume}</td>
         </tr>
+        
     )
 }
 
--- src/components/TableCoins.js
+++ src/components/TableCoins.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import CoinRow from './CoinRow';
 
+const titles = ['#', 'Coin', 'Price', 'Price Change', '24h Volume'];
+
 const TableCoins = ({coins}) => {
     return (
         <table className='table table-dark mt-4 table-hover'>
             <thead>
                 <tr>
-                    <td>
-                        name
-                    </td>
+                   {
+                       titles.map(title => (
+
+                        <td>{title}</td>
+
+                       ))
+                   }
                 </tr>
             </thead>
             <tbody>
                 {coins.map((coin, index) => (
-                <CoinRow coin={coin} key={index} />
+                <CoinRow coin={coin} key={index} index={index} />
                 ))}
             </tbody>
         </table>
